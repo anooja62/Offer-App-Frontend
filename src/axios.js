8e067f06b1b7d124cdc763f6b032ac685909a92b
anooja62 <anooja7777@gmail.com>
2023-03-23 15:19:40 +0530

    updated

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const instance = axios.create({
-    baseURL : 'https://offer-create-app-api.onrender.com',
+    baseURL : 'https://offer-create-app-api.onrender.com/',
 })
 
 
