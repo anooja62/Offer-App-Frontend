3e01c7374b803b7fc4ff5aa351950fa02a9156a6
anooja62 <anooja7777@gmail.com>
2023-03-23 16:27:34 +0530

    updated axios

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,11 @@
-import axios from 'axios'
-
+import axios from 'axios';
 
 const instance = axios.create({
-    baseURL : 'https://offer-create-app-api.onrender.com/',
-})
+  baseURL: 'https://offer-create-app-api.onrender.com/',
+  headers: {
+    'Content-Type': 'application/json',
+    
+  },
+});
 
-
-export default  instance
+export default instance;
